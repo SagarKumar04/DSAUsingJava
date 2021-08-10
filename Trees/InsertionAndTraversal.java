317e6b8347fb455f5a0a1e54d1ca3072aeca2289
SagarKumar04 <31155327+SagarKumar04@users.noreply.github.com>
2021-08-10 22:58:46 +0530

    Add files via upload

--- Trees/InsertionAndTraversal.java
+++ Trees/InsertionAndTraversal.java
@@ -1,5 +1,8 @@
 package InfinityJune21.Trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InsertionAndTraversal {
     static Node root;
     static class Node {
@@ -12,27 +15,129 @@ public class InsertionAndTraversal {
         }
     }
 
+    static void insert(int key) {
+        Node node = new Node(key);
+
+        if(root == null) {
+            root = node;
+        }
+        else {
+            Node temp = root;
+
+            Queue<Node> queue = new LinkedList<Node>();
+            queue.add(temp);
+
+            while(!queue.isEmpty()) {
+                temp = queue.poll();
+
+                if(temp.left == null) {
+                    temp.left = node;
+                    break;
+                }
+                else {
+                    queue.add(temp.left);
+                }
+
+                if(temp.right == null) {
+                    temp.right = node;
+                    break;
+                }
+                else {
+                    queue.add(temp.right);
+                }
+            }
+        }
+    }
+
     static void inorderTraversal(Node temp) {
-        if(temp == null) {
+        if(root == null) {
+            System.out.println("Tree is empty");
+        }
+        else if(temp == null) {
 
         }
         else {
             inorderTraversal(temp.left);
-            System.out.println(temp.key + " ");
+            System.out.print(temp.key + " ");
             inorderTraversal(temp.right);
+        }
+    }
+
+    static void preorderTraversal(Node temp) {
+        if(root == null) {
+            System.out.println("Tree is empty");
+        }
+        else if(temp == null) {
+
+        }
+        else {
+            System.out.print(temp.key + " ");
+            preorderTraversal(temp.left);
+            preorderTraversal(temp.right);
+        }
+    }
+
+    static void postorderTraversal(Node temp) {
+        if(root == null) {
+            System.out.println("Tree is empty");
+        }
+        else if(temp == null) {
+
+        }
+        else {
+            postorderTraversal(temp.left);
+            postorderTraversal(temp.right);
+            System.out.print(temp.key + " ");
+        }
+    }
+
+    static void levelOrderTraversal() {
+        if(root == null) {
+            System.out.println("Tree is empty");
+        }
+        else {
+            Node temp = root;
+
+            Queue<Node> queue = new LinkedList<Node>();
+            queue.add(temp);
+            while(!queue.isEmpty()) {
+                temp = queue.poll();
+                System.out.print(temp.key + " ");
+
+                if(temp.left != null) {
+                    queue.add(temp.left);
+                }
+                if(temp.right != null) {
+                    queue.add(temp.right);
+                }
+            }
         }
     }
 
     public static void main(String[] args) {
         //First node
-        root = new Node(10);
-        root.left = new Node(20);
-        root.right = new Node(30);
-        root.left.left = new Node(40);
-        root.left.right = new Node(50);
-        root.right.left = new Node(60);
-        root.right.right = new Node(70);
+        insert(10);
+        insert(20);
+        insert(30);
+        insert(40);
+        insert(50);
+        insert(60);
+        insert(70);
 
+        System.out.println("Level Order: ");
+        levelOrderTraversal();
+        System.out.println();
+
+        System.out.println("Preorder: ");
+        preorderTraversal(root);
+        System.out.println();
+
+        System.out.println("Inorder: ");
         inorderTraversal(root);
+        System.out.println();
+
+        System.out.println("Postorder: ");
+        postorderTraversal(root);
+        System.out.println();
     }
 }
